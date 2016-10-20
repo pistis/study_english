bc06fdcd6d0f2cc189a26d74fe0854e4997d5fc5
pistis <vamalboro@gmail.com>
2016-10-20 11:39:24 +0900

    implement grid layout

--- src/components/card/CardList.js
+++ src/components/card/CardList.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import InfiniteGrid from 'react-infinite-grid';
+import { CSSGrid, layout } from 'react-stonecutter';
 import { Card } from 'components';
 
 class CardList extends React.Component {
     render(){
         const mapToComponents = data =>{
-            return data.map((card) =>{
-                return (<Card
+            return data.map((card, i) =>{
+                return (<li key={i} itemHeight={150}><Card
                     data={card}
                     ownership={ (card.writer === this.props.currentUser) }
                     key={card._id}
-                />);
+                /></li>);
             });
         };
 
         return (
-            <div className="row">
-                <div className="col-md-12">
-                    {mapToComponents(this.props.data)}
-                </div>
-            </div>
+            <CSSGrid
+                className="grid"
+                component="ul"
+                columns={4}
+                columnWidth={200}
+                gutterWidth={4}
+                gutterHeight={4}
+                layout={layout.pinterest}
+                duration={800}
+                easing="ease-out">
+                {mapToComponents(this.props.data)}
+            </CSSGrid>
         );
     }
 }
